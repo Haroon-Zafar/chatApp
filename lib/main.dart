e2fd98c00b67640879200b2e66dbc5f14ac006db
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-23 15:24:34 +0500

    Firebase.initializeApp();
    import 'package:firebase_core/firebase_core.dart';

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 // Why using Future ? to integrate FireBase
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(MyApp());
 }
