f1cebb7935f15789b672fc594c1cef0f897c05bb
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-23 23:16:53 +0500

    Importing LoginScreen() class in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'LoginScreen.dart';
+
 // Why using Future ? to integrate FireBase
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
