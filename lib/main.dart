9268f4ba17a061e461498efb42f9b31f92a81132
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-23 15:25:25 +0500

    Creating MyApp() , widget-less class

--- lib/main.dart
+++ lib/main.dart
@@ -7,3 +7,12 @@ Future main() async {
   Firebase.initializeApp();
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
