1ddabcd0bcf58e2e456982b0f987560c32867d3b
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-24 18:12:04 +0500

    Placed a SingleChildScrollView() for scrolling.

--- lib/CreateAccount.dart
+++ lib/CreateAccount.dart
@@ -5,6 +5,10 @@ class CreateAccount extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(),
+      ),
+    );
   }
 }
