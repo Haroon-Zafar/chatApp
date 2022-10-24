7078c64646b0de487c6e9da0442fbe302e1b130a
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-24 17:42:44 +0500

    Welcome and Sign in to Continue Text.

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -21,19 +19,33 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
           Container(
             alignment: Alignment.centerLeft,
-            width: size.width,
+            width: size.width / 1.2,
             child: IconButton(
               onPressed: () {},
               icon: Icon(Icons.arrow_back_ios),
             ),
           ),
+          SizedBox(
+            height: size.height / 50,
+          ),
           Container(
-            width: size.width / 1.1,
+            width: size.width / 1.5,
             child: Text(
               "Welcome",
               style: TextStyle(
                 fontSize: 28,
                 fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            width: size.width / 1.3,
+            child: Text(
+              "Sign In to Continue",
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 25,
+                fontWeight: FontWeight.w500,
               ),
             ),
           ),
