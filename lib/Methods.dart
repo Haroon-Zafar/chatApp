efe0542a07a0881acd1e255976ae2b7efbe9d4b9
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-25 18:36:53 +0500

    FirebaseAuth _auth variable is created.
    It will help us to call FirebaseAuth class's functions.

--- lib/Methods.dart
+++ lib/Methods.dart
@@ -1,3 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-Future<User> createAccount() async {}
+Future createAccount(String name, String email, String password) async {
+  // IT will help us to call FirebaseAuth class's functions.
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  try {} catch (e) {}
+}
