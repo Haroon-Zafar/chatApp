e38c15c40d3c8ff7278e2563789a2bf7f1584b5f
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-10-25 18:42:31 +0500

    if else logic for user login.

--- lib/Methods.dart
+++ lib/Methods.dart
@@ -4,5 +4,21 @@ Future createAccount(String name, String email, String password) async {
   // IT will help us to call FirebaseAuth class's functions.
   FirebaseAuth _auth = FirebaseAuth.instance;
 
-  try {} catch (e) {}
+  try {
+    // make user nullable User -> User?
+    User? user = (await _auth.createUserWithEmailAndPassword(
+            email: email, password: password))
+        .user;
+
+    if (user != null) {
+      print("Login Successful");
+      return user;
+    } else {
+      print("Account Creation failed");
+      return user;
+    }
+  } catch (e) {
+    print(e);
+    return null;
+  }
 }
